8d06e81448931745560d5749584a0fc80173b405
Slavey Karadzhov <slaff@linux-bg.org>
2013-05-09 08:08:35 +0200

    Initial version of the User module.
    tags:[ch-account-controller]

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,9 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application','Debug',
+        'Application',
+    	'Debug',
+    	'User',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
