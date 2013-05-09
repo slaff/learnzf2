3ce6f16770304ba228c128cf2d63b88dc8d1cdbd
Slavey Karadzhov <slaff@linux-bg.org>
2013-05-08 23:59:19 -0400

    Added dispatch error event listener.
    tags:[ch-events]

--- module/Debug/Module.php
+++ module/Debug/Module.php
@@ -2,10 +2,10 @@
 namespace Debug;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\Mvc\ModuleRouteListener;
 use Zend\ModuleManager\ModuleManager;
 use Zend\ModuleManager\ModuleEvent;
 use Zend\EventManager\Event;
+use Zend\Mvc\MvcEvent;
 
 class Module implements AutoloaderProviderInterface
 {
@@ -42,12 +42,23 @@ class Module implements AutoloaderProviderInterface
     	error_log(var_export($loadedModules, true));
     }
 
-    public function onBootstrap($e)
-    {
-        // You may not need to do this if you're doing it elsewhere in your
-        // application
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
+	public function onBootstrap(MvcEvent $e)
+	{
+		$eventManager        = $e->getApplication()->getEventManager();
+		$eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'handleError'));
+	}
+    
+    public function handleError(MvcEvent $event) {
+    	$controller = $event->getController();
+    	$error      = $event->getParam('error');
+    	$exception  = $event->getParam('exception');
+    	$message = sprintf('Error dispatching controller "%s". Error was: "%s"', $controller, $error);
+    	if ($exception instanceof \Exception) {
+    		$message .= ', Exception('.$exception->getMessage().'): '.
+    				$exception->getTraceAsString();
+    	}
+    		
+    	error_log($message);
+    		
     }
 }
