06138ac4f60b204742893a1d1125362107993bdf
Slavey Karadzhov <slaff@linux-bg.org>
2013-05-09 12:09:19 +0200

    Initial version of the Table Gateway Service and demo how to use it in the AccountController.
    tags:[ch-database-tablegateway]

---
+++ module/User/src/User/Service/Invokable/TableGateway.php
@@ -0,0 +1,60 @@
+<?php 
+namespace User\Service\Invokable;
+
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Db\TableGateway\TableGateway as DbTableGateway;
+
+class TableGateway implements ServiceLocatorAwareInterface 
+{
+	/**
+	 * @var array
+	 */
+	protected $cache;
+	
+    /**
+     * @var ServiceLocatorInterface
+     */
+    protected $serviceLocator;
+    
+    public function get($tableName, $features=null, $resultSetPrototype=null)
+    {
+    	$cacheKey = $tableName;
+    	// $cacheKey = md5(serialize($tableName.$features.$resultSetPrototype));
+    	if(isset($this->cache[$key])) {
+    		return $this->cache;
+    	}
+    	
+    	$config  = $this->serviceLocator->get('config');
+    	// defined which class should be used for which table
+    	$tableGatewayMap = $config['table-gateway']['map'];
+    	if(isset($tableGatewayMap[$tableName])) {
+            $className = $tableGatewayMap[$tableName];
+            $this->cache[$cacheKey] = new $className();
+        }
+        else {
+            $db = $this->serviceLocator->get('database');
+            $this->cache[$cacheKey] = new DbTableGateway($tableName, $db, $features, $resultSetPrototype);
+        }
+        
+        return $this->cache[$cacheKey];
+    }
+    
+	/* (non-PHPdoc)
+	 * @see \Zend\ServiceManager\ServiceLocatorAwareInterface::setServiceLocator()
+	 */
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator) 
+	{
+		$this->serviceLocator = $serviceLocator;
+		
+	}
+
+	/* (non-PHPdoc)
+	 * @see \Zend\ServiceManager\ServiceLocatorAwareInterface::getServiceLocator()
+	 */
+	public function getServiceLocator() 
+	{
+		$this->serviceLocator;
+	}
+
+}
