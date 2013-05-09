4d32930e5b61c6b27158f1adfdbaed9be81c9f39
Slavey Karadzhov <slaff@linux-bg.org>
2013-05-09 09:08:42 +0200

    Simple usage and visualization of the user form.

--- module/User/src/User/Controller/AccountController.php
+++ module/User/src/User/Controller/AccountController.php
@@ -1,40 +1,55 @@
 <?php
 namespace User\Controller;
 
+use User\Form\User as UserForm;
 use Zend\Mvc\Controller\AbstractActionController;
 
-class AccountController extends AbstractActionController
+class AccountController extends AbstractActionController 
 {
-    public function indexAction()
+	public function indexAction() 
+	{
+		return array();
+	}
+
+	public function addAction()
     {
-        return array();
+        $form = new UserForm();
+        if($this->getRequest()->isPost()) {
+            $data = array_merge_recursive(
+                $this->getRequest()->getPost()->toArray(),
+                // Notice: make certain to merge the Files also to the post data
+                $this->getRequest()->getFiles()->toArray()
+            );
+            $form->setData($data);
+            if($form->isValid()) {
+            	// @todo: save the data of the new user 
+            }
+        }
+        
+        // pass the data to the view for visualization
+        return array('form1'=> $form);
     }
 
-    public function addAction()
-    {
-        return array();
-    }
+	/*
+	 * Anonymous users can use this action to register new accounts
+	 */
+	public function registerAction() 
+	{
+		return array();
+	}
 
-  /*
-   * Anonymous users can use this action to register new accounts
-   */
-  public function registerAction() 
-  {
-    return array();
-  }
-    
-    public function viewAction()
-    {
-        return array();
-    }
-    
-    public function editAction()
-    {
-        return array();
-    }
-    
-    public function deleteAction()
-    {
-        return array();
-    }
+	public function viewAction() 
+	{
+		return array();
+	}
+
+	public function editAction() 
+	{
+		return array();
+	}
+
+	public function deleteAction() 
+	{
+		return array();
+	}
 }
