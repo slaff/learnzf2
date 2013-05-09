3609e45bd4be8d6029f1dcfca0ffc1eae0964c2e
Slavey Karadzhov <slaff@linux-bg.org>
2013-05-09 08:08:35 +0200

    Initial version of the User module.
    tags:[ch-account-controller]

---
+++ module/User/config/module.config.php
@@ -0,0 +1,51 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+           // below is key              and below is the fully qualified class name
+           'User\Controller\Account' => 'User\Controller\AccountController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'user' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    // Change this to something specific to your module
+                    'route'    => '/user',
+                    'defaults' => array(
+                        // Change this value to reflect the namespace in which
+                        // the controllers for your module are found
+                        '__NAMESPACE__' => 'User\Controller',
+                        'controller'    => 'Account',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    // This route is a sane default when developing a module;
+                    // as you solidify the routes for your module, however,
+                    // you may want to remove it and replace it with more
+                    // specific routes.
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'User' => __DIR__ . '/../view',
+        ),
+    ),
+);
---
+++ module/User/src/User/Controller/AccountController.php
@@ -0,0 +1,40 @@
+<?php
+namespace User\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class AccountController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+    public function addAction()
+    {
+        return array();
+    }
+
+  /*
+   * Anonymous users can use this action to register new accounts
+   */
+  public function registerAction() 
+  {
+    return array();
+  }
+    
+    public function viewAction()
+    {
+        return array();
+    }
+    
+    public function editAction()
+    {
+        return array();
+    }
+    
+    public function deleteAction()
+    {
+        return array();
+    }
+}
