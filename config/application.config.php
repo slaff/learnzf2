afa5e89cadbdc5a4c7a202329af5bc45b7727b84
Slavey Karadzhov <slavey.karadzhov@zend.com>
2013-06-02 00:10:26 +0200

    Enabling the caching of configurations.

--- config/application.config.php
+++ config/application.config.php
@@ -29,21 +29,21 @@ return array(
         // Whether or not to enable a configuration cache.
         // If enabled, the merged configuration will be cached and used in
         // subsequent requests.
-        //'config_cache_enabled' => $booleanValue,
+        'config_cache_enabled' => true,
 
         // The key used to create the configuration cache file name.
-        //'config_cache_key' => $stringKey,
+        'config_cache_key' => '1',
 
         // Whether or not to enable a module class map cache.
         // If enabled, creates a module class map cache which will be used
         // by in future requests, to reduce the autoloading process.
-        //'module_map_cache_enabled' => $booleanValue,
+        'module_map_cache_enabled' => true,
 
         // The key used to create the class map cache file name.
-        //'module_map_cache_key' => $stringKey,
+        'module_map_cache_key' => '1',
 
         // The path in which to cache merged configuration.
-        //'cache_dir' => $stringPath,
+        'cache_dir' => 'data/cache/module',
 
         // Whether or not to enable modules dependency checking.
         // Enabled by default, prevents usage of modules that depend on other modules
