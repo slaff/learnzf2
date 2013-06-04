bb3adcb85cde3f6332c3bf09551ecd319f9a47f8
Slavey Karadzhov <slavey.karadzhov@zend.com>
2013-06-04 18:42:17 +0200

    Used the Doctrine 2 entity manager to save the data coming from the form.

--- module/User/src/User/Controller/AccountController.php
+++ module/User/src/User/Controller/AccountController.php
@@ -4,6 +4,7 @@ namespace User\Controller;
 use User\Model\User as UserModel;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Form\Annotation\AnnotationBuilder;
+use Zend\EventManager\EventManager;
 
 class AccountController extends AbstractActionController
 {
@@ -14,49 +15,49 @@ class AccountController extends AbstractActionController
 
     public function addAction()
     {
-    	// The annotation builder help us create a form from the annotations in the user entity.
-    	$builder = new AnnotationBuilder();
-    	$entity = $this->serviceLocator->get('user-entity');
-    	$form = $builder->createForm($entity);
-    	
-    	$form->add(array(
-    			'name' => 'password_verify',
-    			'type' => 'Zend\Form\Element\Password',
-    			'attributes' => array(
-    					'placeholder' => 'Verify Password Here...',
-    					'required' => 'required',
-    			),
-    			'options' => array(
-    					'label' => 'Verify Password',
-    			)
-    	),
-    			array (
-    					'priority' => $form->get('password')->getOption('priority'),
-    			)
-    	);
-    	
-    	// This is the special code that protects our form being submitted from automated scripts
-    	$form->add(array(
-    			'name' => 'csrf',
-    			'type' => 'Zend\Form\Element\Csrf',
-    	));
-    	
-    	// This is the submit button
-    	$form->add(array(
-    			'name' => 'submit',
-    			'type' => 'Zend\Form\Element\Submit',
-    			'attributes' => array(
-    					'value' => 'Submit',
-    					'required' => 'false',
-    			),
-    	));
-    	
-    	// We bind the entity to the user. If the form tries to read/write data from/to the entity 
-    	// it will use the hydrator specified in the entity to achieve this. In our case we use ClassMethods
-    	// hydrator which means that reading will happen calling the getter methods and writing will happen by
-    	// calling the setter methods.
-    	$form->bind($entity);
-    	
+        // The annotation builder help us create a form from the annotations in the user entity.
+        $builder = new AnnotationBuilder();
+        $entity = $this->serviceLocator->get('user-entity');
+        $form = $builder->createForm($entity);
+
+        $form->add(array(
+                'name' => 'password_verify',
+                'type' => 'Zend\Form\Element\Password',
+                'attributes' => array(
+                        'placeholder' => 'Verify Password Here...',
+                        'required' => 'required',
+                ),
+                'options' => array(
+                        'label' => 'Verify Password',
+                )
+        ),
+                array (
+                        'priority' => $form->get('password')->getOption('priority'),
+                )
+        );
+
+        // This is the special code that protects our form being submitted from automated scripts
+        $form->add(array(
+                'name' => 'csrf',
+                'type' => 'Zend\Form\Element\Csrf',
+        ));
+
+        // This is the submit button
+        $form->add(array(
+                'name' => 'submit',
+                'type' => 'Zend\Form\Element\Submit',
+                'attributes' => array(
+                        'value' => 'Submit',
+                        'required' => 'false',
+                ),
+        ));
+
+        // We bind the entity to the user. If the form tries to read/write data from/to the entity
+        // it will use the hydrator specified in the entity to achieve this. In our case we use ClassMethods
+        // hydrator which means that reading will happen calling the getter methods and writing will happen by
+        // calling the setter methods.
+        $form->bind($entity);
+
         if($this->getRequest()->isPost()) {
             $data = array_merge_recursive(
                 $this->getRequest()->getPost()->toArray(),
@@ -65,20 +66,23 @@ class AccountController extends AbstractActionController
             );
             $form->setData($data);
             if($form->isValid()) {
-                // You can use the standard way of instantiating a table gateway
-                //$model = new UserModel();
-                // Or if you have many db tables that do need special treatment of the incoming data
-                // you can use the table gateway service
-                $model = $this->serviceLocator->get('table-gateway')->get('users');
-                $id = $model->insert($form->getData());
+                // We use now the Doctrine 2 entity manager to save user data to the database
+                $entityManager = $this->serviceLocator->get('entity-manager');
+                $entityManager->persist($entity);
+                $entityManager->flush();
 
                 $this->flashmessenger()->addSuccessMessage('User was added successfully.');
+
+                $event = new EventManager('user');
+                $event->trigger('register', $this, array(
+                    'user'=> $entity,
+                ));
 
                 // redirect the user to the view user action
                 return $this->redirect()->toRoute('user/default', array (
                         'controller' => 'account',
                         'action'     => 'view',
-                        'id'		 => $id
+                        'id'		 => $entity->getId()
                 ));
             }
         }
