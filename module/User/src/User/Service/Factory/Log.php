7f720a2c8f2ff2fb359b88bd3f24f6240761045d
Slavey Karadzhov <slavey.karadzhov@zend.com>
2013-06-05 10:32:11 +0200

    Finished the initial logging code.
    tags:[ch-logger]

--- module/User/src/User/Service/Factory/Log.php
+++ module/User/src/User/Service/Factory/Log.php
@@ -5,6 +5,7 @@ use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\Log\Logger;
 use Zend\Log\Writer\Stream as StreamWriter;
+use Zend\Log\Filter\Priority as PriorityFilter;
 
 class Log implements FactoryInterface
 {
@@ -17,7 +18,11 @@ class Log implements FactoryInterface
         $writer = new StreamWriter('php://stderr');
         $log->addWriter($writer);
 
-
+        $priority = @$config['log']['priority'];
+        if ($priority!==null) {
+            $filter = new PriorityFilter($priority);
+            $writer->addFilter($filter);
+        }
         return $log;
     }
 }
