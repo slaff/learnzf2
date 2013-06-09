e68968b65d481b584a683a02087a888802b03c5d
Slavey Karadzhov <slavey.karadzhov@zend.com>
2013-06-09 11:33:38 +0200

    Applied automatic coding style checker. tags:[ch-cs]

--- module/User/src/User/Controller/AccountController.php
+++ module/User/src/User/Controller/AccountController.php
@@ -5,14 +5,14 @@ use User\Form\User as UserForm;
 use User\Model\User as UserModel;
 use Zend\Mvc\Controller\AbstractActionController;
 
-class AccountController extends AbstractActionController 
+class AccountController extends AbstractActionController
 {
-	public function indexAction() 
-	{
-		return array();
-	}
+    public function indexAction()
+    {
+        return array();
+    }
 
-	public function addAction()
+    public function addAction()
     {
         $form = new UserForm();
         if($this->getRequest()->isPost()) {
@@ -23,68 +23,68 @@ class AccountController extends AbstractActionController
             );
             $form->setData($data);
             if($form->isValid()) {
-            	// You can use the standard way of instantiating a table gateway 
-            	//$model = new UserModel();
-            	// Or if you have many db tables that do need special treatment of the incoming data
-            	// you can use the table gateway service
-            	$model = $this->serviceLocator->get('table-gateway')->get('users');
-            	$id = $model->insert($form->getData());
-            	
-            	$this->flashmessenger()->addSuccessMessage('User was added successfully.');
-            	
-            	// redirect the user to the view user action
-            	return $this->redirect()->toRoute('user/default', array (
-	            	    'controller' => 'account',
-	            	    'action'     => 'view',
-            			'id'		 => $id
-            	));
+                // You can use the standard way of instantiating a table gateway
+                //$model = new UserModel();
+                // Or if you have many db tables that do need special treatment of the incoming data
+                // you can use the table gateway service
+                $model = $this->serviceLocator->get('table-gateway')->get('users');
+                $id = $model->insert($form->getData());
+
+                $this->flashmessenger()->addSuccessMessage('User was added successfully.');
+
+                // redirect the user to the view user action
+                return $this->redirect()->toRoute('user/default', array (
+                        'controller' => 'account',
+                        'action'     => 'view',
+                        'id'		 => $id
+                ));
             }
         }
-        
+
         // pass the data to the view for visualization
         return array('form1'=> $form);
     }
 
-	/*
-	 * Anonymous users can use this action to register new accounts
-	 */
-	public function registerAction() 
-	{
-		$result = $this->forward()->dispatch('User\Controller\Account', array(
+    /*
+     * Anonymous users can use this action to register new accounts
+     */
+    public function registerAction()
+    {
+        $result = $this->forward()->dispatch('User\Controller\Account', array(
             'action' => 'add',
         ));
-        
+
         return $result;
-	}
+    }
 
-	public function viewAction() 
-	{
-		return array();
-	}
+    public function viewAction()
+    {
+        return array();
+    }
 
-	public function editAction() 
-	{
-		return array();
-	}
+    public function editAction()
+    {
+        return array();
+    }
 
-	public function deleteAction() 
-    {   
+    public function deleteAction()
+    {
         $id = $this->params('id');
         if(!$id) {
-        	return $this->redirect()->toRoute('user/default', array(
-        	    'controller' => 'account',
-        	    'action' => 'view',
-        	));
+            return $this->redirect()->toRoute('user/default', array(
+                'controller' => 'account',
+                'action' => 'view',
+            ));
         }
     }
-    
+
     public function meAction()
-    {	
-    	return array();
+    {
+        return array();
     }
-    
+
     public function deniedAction()
     {
-    	return array();
+        return array();
     }
 }
--- module/User/tests/User/Controller/AccountControllerTest.php
+++ module/User/tests/User/Controller/AccountControllerTest.php
@@ -4,47 +4,47 @@ namespace UserTest\Controller;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 use Zend\Db\Adapter\Adapter;
 
-class AccountControllerTest extends AbstractHttpControllerTestCase 
+class AccountControllerTest extends AbstractHttpControllerTestCase
 {
     protected $traceError = true;
-    
+
     public function setUp()
     {
     	$this->setApplicationConfig(
-    			include __DIR__.'/../../config/application.config.php'
-    	);
-    	parent::setUp();
+                include __DIR__.'/../../config/application.config.php'
+        );
+        parent::setUp();
     	
-    	$serviceManager = $this->getApplication()->getServiceManager();
-    	$db = $serviceManager->get('database');
-    	$sql  = file_get_contents(__DIR__.'/../../../sql/build.sqlite.sql')."\n" .
-    	file_get_contents(__DIR__.'/../../samples/users.sql');
-    	$queries = preg_split('/;(\s*)\n/', $sql);
-    	foreach ($queries as $query) {
-    	   $db->query($query, Adapter::QUERY_MODE_EXECUTE);
-    	}
+        $serviceManager = $this->getApplication()->getServiceManager();
+        $db = $serviceManager->get('database');
+        $sql  = file_get_contents(__DIR__.'/../../../sql/build.sqlite.sql')."\n" .
+        file_get_contents(__DIR__.'/../../samples/users.sql');
+        $queries = preg_split('/;(\s*)\n/', $sql);
+        foreach ($queries as $query) {
+                $db->query($query, Adapter::QUERY_MODE_EXECUTE);
+        }
     }
-    
+
     public function testMeAction()
     {
-    	$application = $this->getApplication();
-    	$serviceManager = $application->getServiceManager();
-    	$eventManager   = $application->getEventManager();
-    	
-    	// This is how the request object can be accessed and modified.
-    	$request = $this->getRequest();
-    	
-    	// The dispatch method returns the result. 
-    	$result = $this->dispatch('/user/account/me');
-    	
-    	 
-    	$this->assertActionName('me');
-    	$this->assertControllerName('User\Controller\Account');
-    	
-    	// This is how the response object can be accessed.
-    	$response = $this->getResponse();
-    	
-    	// And here we can use the response to check the status code.
-    	$this->assertEquals(200, $response->getStatusCode());
+        $application = $this->getApplication();
+        $serviceManager = $application->getServiceManager();
+        $eventManager   = $application->getEventManager();
+
+        // This is how the request object can be accessed and modified.
+        $request = $this->getRequest();
+
+        // The dispatch method returns the result.
+        $result = $this->dispatch('/user/account/me');
+
+
+        $this->assertActionName('me');
+        $this->assertControllerName('User\Controller\Account');
+
+        // This is how the response object can be accessed.
+        $response = $this->getResponse();
+
+        // And here we can use the response to check the status code.
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
